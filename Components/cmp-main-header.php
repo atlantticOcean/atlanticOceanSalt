05801f4e1483036aa6a227e8e73a7bb355dbc1e9
atlantticOcean <avatarmedo@yahoo.ca>
2024-05-20 00:12:15 -0400

    Add files via upload

---
+++ Components/cmp-main-header.php
@@ -0,0 +1,12 @@
+<header>
+    <nav class="headerNav">
+        <div class="headerTitle">
+            Atlantic Ocean Salt
+        </div>
+        <div class="headerButtons">
+            <a href="landing.php" id="homeBtn" class="headerBtn"> </a>
+            <a href="contact.php" id="contactBtn" class="headerBtn"></a>
+
+        </div>
+    </nav>
+</header>
